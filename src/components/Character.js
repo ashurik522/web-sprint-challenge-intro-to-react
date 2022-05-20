6196198300518cd109ca9e8238206623296aa03e
ashurik522 <ashurik522@gmail.com>
2022-05-20 10:17:39 -0400

    data population

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,22 @@
 // Write your Character component here
 import styled from 'styled-components';
-
+import React from 'react';
 const StyledCards = styled.div`
-
+    
 `
 
 const Character = props => {
+const {characters} = props
+
     return(
         <StyledCards>
             <div className='card-container'>
-                <h1>{props.characters.name}</h1>
-            </div>
+                <h1>{characters.name} </h1>
+                <h2>Height: {characters.height} cm </h2>
+                <h2>Weight: {characters.mass} kg </h2>
+                <h2>Gender: {characters.gender}</h2>
+                <h2>Hair Color: {characters.hair_color}</h2>
+            </div>        
         </StyledCards>
     );
 };
